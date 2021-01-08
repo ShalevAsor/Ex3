82b9ab8fefb0a711608bb23c5922169d882cb031
swiftypanties <sonyon.winx@gmail.com>
2021-01-08 16:27:55 +0200

    changed titles

--- src/DataComparison.py
+++ src/DataComparison.py
@@ -4,23 +4,23 @@ import numpy as np
 
 if __name__ == '__main__':
     # ---------------shortest_path--------------#
-    # title = "shortest path run time"
+    # title = "Shortest Path"
     # labels = ['G10', 'G1K', 'G20K', 'G30K']
     # nx_runtime = [0.0, 0.004000, 0.120001, 0.197962]
     # ga_runtime = [0.0, 0.009001, 0.320001, 0.662059]
     # ga_java_runtime = [0, 0.001, 0.221,0.316]
     # -----------connected_components-----------#
-    title="node's strongly connected component"
-    labels = ['G10', 'G1K', 'A5']
-    nx_runtime = [0.0, 0.015972, 0.0]
-    ga_runtime = [0.003988, 0.008812, 0.0,]
-    ga_java_runtime = [0, 0, 0,]
+    # title="Strong connectivity group of node"
+    # labels = ['G10', 'G1K', 'A5']
+    # nx_runtime = [0.0, 0.015972, 0.0]
+    # ga_runtime = [0.003988, 0.008812, 0.0,]
+    # ga_java_runtime = [0, 0, 0,]
     # -----------connected_component-----------#
-    # title="connected_component run time"
-    # labels = ['G10', 'G1K', 'A5',]
-    # nx_runtime = [0.0, 0.021676,0.001008]
-    # ga_runtime = [0.0, 0.014198, 0.0]
-    # ga_java_runtime = [0, 0, 0.009,]
+    title="Num of strongly connected components"
+    labels = ['G10', 'G1K', 'A5',]
+    nx_runtime = [0.0, 0.021676,0.001008]
+    ga_runtime = [0.0, 0.014198, 0.0]
+    ga_java_runtime = [0, 0, 0.009,]
 
     x = np.arange(len(labels))  # the label locations
     width = 0.2  # the width of the bars
